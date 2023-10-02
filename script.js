15cd17180f9d0f964ec1a5157daefd872a41e3b9
LelleOne <linushagstrom06@gmail.com>
2023-10-02 14:37:36 +0200

    now finnished but can make it look and feel better

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
+document.addEventListener('DOMContentLoaded', function () {
 const guessSubmit = document.querySelector('.guessSubmit');
 const yeh = document.querySelector('.Erm');
 const hintText = document.querySelector('.hints');
+const guessField = document.querySelector('.guessField');
 let hints = 1;
 let resetButton;
 let guessCount = 0;
@@ -15,7 +17,7 @@ const Countries = [
     "austria",
     "belarus",
     "belgium",
-    "bosnia and herzegovina",
+    "bosnia",
     "bulgaria",
     "croatia",
     "cyprus",
@@ -59,64 +61,153 @@ const Countries = [
   ];
 
   const countryFacts = [
-    "Known for its beautiful Adriatic and Ionian coastlines.",
-    "One of the smallest countries in Europe, located in the Pyrenees mountains.",
-    "Famous for its classical music heritage, with composers like Mozart and Beethoven.",
-    "A landlocked country in Eastern Europe known for its diverse wildlife.",
-    "Renowned for its delicious chocolates, waffles, and beer.",
-    "Has a rich cultural heritage and is known for its historic towns.",
-    "Famous for its stunning landscapes, including the Rila Monastery and Black Sea coast.",
-    "Known for its beautiful Adriatic coastline and historic cities like Dubrovnik.",
-    "An island country with a rich history and Mediterranean cuisine.",
-    "Located in Central Europe and known for its picturesque cities and castles.",
-    "Known for its Viking history and high standard of living.",
-    "A Baltic state with a strong digital identity and e-governance.",
-    "Famous for its lakes, forests, and the Northern Lights.",
+    // Albania
+    "Located in Southeastern Europe with a diverse culture and beautiful landscapes.",
+
+    // Andorra
+    "A small landlocked country in the Pyrenees mountains, known for its ski resorts and natural beauty.",
+
+    // Austria
+    "Famous for its classical music heritage and stunning Alpine landscapes.",
+
+    // Belarus
+    "A landlocked country in Eastern Europe, known for its rich history and culture.",
+
+    // Belgium
+    "Known for its delicious chocolates, waffles, and a variety of beers.",
+
+    // Bosnia and Herzegovina
+    "Has a rich cultural heritage and is known for its historic towns and natural attractions.",
+
+    // Bulgaria
+    "Famous for its picturesque landscapes, including beautiful monasteries and a Black Sea coast.",
+
+    // Croatia
+    "Known for its stunning Adriatic coastline, historic cities, and vibrant culture.",
+
+    // Cyprus
+    "An island nation in the Mediterranean, famous for its historical sites and beautiful beaches.",
+
+    // Czech Republic
+    "Located in Central Europe and known for its charming cities and historic architecture.",
+
+    // Denmark
+    "A Nordic country known for its progressive social policies and high standard of living.",
+
+    // Estonia
+    "One of the Baltic states with a strong emphasis on education and technology.",
+
+    // Finland
+    "Famous for its lakes, forests, and the Northern Lights in the north.",
+
+    // France
     "Known for its world-class cuisine, fashion, and art.",
-    "A European economic powerhouse known for its engineering and automotive industries.",
-    "Famous for its ancient history, including the birthplace of democracy.",
+
+    // Germany
+    "A major European powerhouse with a rich history and diverse culture.",
+
+    // Greece
+    "Famous for its ancient history, including the birthplace of democracy in Athens.",
+
+    // Hungary
     "Located in Central Europe and known for its unique language and cuisine.",
+
+    // Iceland
     "A Nordic island country known for its stunning landscapes and geothermal activity.",
-    "Famous for its lush green landscapes and friendly people.",
-    "Known for its delicious food, art, and rich history.",
+
+    // Ireland
+    "Known for its lush green landscapes, friendly people, and rich folklore.",
+
+    // Italy
+    "Famous for its natural beauty, rich history, and distinctive culture.",
+
+    // Kosovo
     "A partially recognized state in the Balkans with a complex political status.",
-    "A Baltic state known for its beautiful beaches and forests.",
-    "A small, landlocked principality in the Alps known for its financial services.",
+
+    // Latvia
+    "One of the Baltic states with beautiful forests and a stunning coastline.",
+
+    // Liechtenstein
+    "A small landlocked principality in the Alps known for its financial services.",
+
+    // Lithuania
     "Famous for its strong basketball tradition and vibrant festivals.",
-    "A small Baltic state with a rich cultural heritage and beautiful landscapes.",
+
+    // Luxembourg
+    "A small Baltic state with a rich cultural heritage and beautiful seaside resorts.",
+
+    // Malta
     "One of the smallest countries in Europe, known for its banking and finance industry.",
-    "An island nation known for its historical sites and Mediterranean culture.",
-    "A landlocked country in Eastern Europe with a rich history and diverse culture.",
-    "A small coastal city-state known for its luxury and casinos.",
-    "A Balkan country known for its stunning Adriatic coastline and mountainous terrain.",
-    "A Western European country known for its picturesque canals and windmills.",
-    "Located in the Balkans and known for its scenic landscapes and historic cities.",
+
+    // Moldova
+    "An island nation with a Mediterranean culture and historical sites.",
+
+    // Monaco
+    "A landlocked country in Eastern Europe with a diverse culture and scenic landscapes.",
+
+    // Montenegro
+    "A glamorous city-state known for its luxury and casinos.",
+
+    // Netherlands
+    "A Balkan country with a stunning Adriatic coastline and mountainous terrain.",
+
+    // North Macedonia
+    "Known for its picturesque canals, tulip fields, and cycling culture.",
+
+    // Norway
+    "Located in the Balkans with beautiful landscapes and historic cities.",
+
+    // Poland
     "A Northern European country with breathtaking fjords and natural beauty.",
+
+    // Portugal
     "Famous for its hearty cuisine, including pierogi and kielbasa.",
+
+    // Romania
     "Known for its maritime history, port wine, and scenic coastline.",
-    "A Southern European country with a rich history and stunning landscapes.",
+
+    // Russia
+    "A Southern European country with a rich history and diverse landscapes.",
+
+    // San Marino
     "Located in Eastern Europe and known for its thermal baths and paprika dishes.",
+
+    // Serbia
     "A Nordic island country known for its innovative technology and renewable energy.",
-    "A landlocked country in the Alps known for its stunning landscapes and banking sector.",
-    "A vast country in Eastern Europe known for its diverse landscapes and history.",
-    "A small landlocked country surrounded by Italy, known for its medieval architecture.",
-    "A Balkan country with a rich cultural heritage and diverse landscapes.",
-    "Located in Central Europe and known for its historic cities and castles.",
-    "A picturesque country in the Balkans known for its natural beauty and historic sites.",
-    "A Western European country known for its sunny beaches and delicious cuisine.",
-    "Located in Northern Europe and known for its innovative design and technology.",
-    "A landlocked country in Central Europe with stunning Alpine scenery.",
-    "A large Eastern European country known for its diverse culture and vast landscapes.",
-    "A country consisting of four nations with a rich history and cultural diversity.",
-    "The world's smallest independent state, known for its religious and historical significance."
+
+    // Slovakia
+    "A landlocked country in the Alps known for its stunning landscapes and financial sector.",
+
+    // Slovenia
+    "A vast country in Eastern Europe with diverse cultures and landscapes.",
+
+    // Spain
+    "A country consisting of four nations, each with its own unique identity and culture.",
+
+    // Sweden
+    "The world's smallest independent state, known for its religious and historical significance.",
+
+     // Switzerland
+     "Known for its stunning Alpine landscapes, precision watchmaking, and chocolate.",
+
+     // Ukraine
+     "The largest country in Europe by land area, known for its diverse culture and history.",
+ 
+     // United Kingdom
+     "Comprising four nations, each with its own identity and culture, and famous for its royal heritage.",
+ 
+     // Vatican City
+     "The world's smallest independent state, the spiritual and administrative center of the Catholic Church."
 ];
   
   const random = Math.floor(Math.random() * Countries.length);
   const randomCountry = (random, Countries[random]);
   const randomFact = (random, countryFacts[random]);
 
-  console.log(random, Countries[random]);
-  console.log(randomFact)
+  console.log(randomCountry);
+  console.log(randomFact);
+  hintText.textContent = randomFact;
+  hintText.style.color = 'green';
 
 
 function checkGuess(){
@@ -124,8 +215,8 @@ function checkGuess(){
     const submitedGuess = userGuess.toLowerCase();
 
     if(guessCount === 0){
-        guessCount.textContent = 'Ermmm'
-        hintText.textContent = randomFact
+        yeh.textContent = 'Ermmm'
+        
     }
     
     if(submitedGuess === randomCountry){
@@ -150,3 +241,30 @@ function checkGuess(){
 }
 
 guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+
+  function resetGame() {
+    guessCount = 1;
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (const resetPara of resetParas) {
+      resetPara.textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+    yeh.style.backgroundColor = '#292929';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    yeh.textContent = '';
+  }
+});
